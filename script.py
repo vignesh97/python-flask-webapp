49408b392b7b17ffababb6b069d66cc9c6978aeb
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-03-17 15:13:12 -0400

    Adding additional route Python

--- script.py
+++ script.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Website content goes here"
 
+@app.route('/about/')
+def home():
+    return "About Page"
+
 if __name__=="__main__":
     app.run(debug=True)
